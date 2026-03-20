7ced75af8a0fd84be9e7d8df7c62b07802da7d11
isOdin-l <ivanbildagin@mail.ru>
2026-03-20 23:55:09 +0300

    feat: added ticker to stack-service and led it to work status

--- pkg/db_models/schema/queries/stack.sql
+++ pkg/db_models/schema/queries/stack.sql
@@ -1,11 +1,11 @@
 -- name: FindMatches :many
 SELECT DISTINCT f2.profile_id
 FROM fav_art_styles f1
-JOIN fav_art_styles f2 ON f1.style_id = f2.style_id
+JOIN fav_art_styles f2 ON f1.style = f2.style
 JOIN profiles p2 ON p2.id = f2.profile_id
 JOIN profiles p1 ON p1.id = $1
 JOIN preferences pref ON pref.profile_id = $1
-WHERE f1.profile_id = $1 AND f2.profile_id != $1 AND ST_DWithin(p1.location, p2.location, pref.max_distance);
+WHERE f1.profile_id = $1 AND f2.profile_id != $1 AND ST_DWithin(p1.location, p2.location, pref.max_distance_meters);
 
 -- name: GetAllProfiles :many
 SELECT id FROM profiles;
--- services/stack/config/config.go
+++ services/stack/config/config.go
@@ -8,6 +8,13 @@ import (
 type Config struct {
 	configs.ConfigRedis
 	configs.ConfigPostgres
+	InternalConfig
+}
+
+type InternalConfig struct {
+	// That variable used to set ticker, which rule after what period of hours
+	// stack generation should be
+	TickPeriodHours int64 `env:"TICK_PERIOD_HOURS"`
 }
 
 func NewConfig() (Config, error) {
